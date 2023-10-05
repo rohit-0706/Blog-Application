9ec0c919a775aab19cf0c351b0e013159f1697b7
rohitverma <rv16verma@gmail.com>
2023-10-05 17:13:43 +0530

    first commit

---
+++ src/main/java/com/rohit/javablogapplication/JavaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.javablogapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBlogApplication.class, args);
+	}
+
+}
