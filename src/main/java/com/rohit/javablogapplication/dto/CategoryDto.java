3110641869cea7ca8c4b0234af723565d4108535
rohitverma <rv16verma@gmail.com>
2023-11-16 22:44:49 +0530

    added validation

--- src/main/java/com/rohit/javablogapplication/dto/CategoryDto.java
+++ src/main/java/com/rohit/javablogapplication/dto/CategoryDto.java
@@ -1,6 +1,8 @@
 package com.rohit.javablogapplication.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +12,10 @@ import lombok.Setter;
 @Setter
 public class CategoryDto {
     private String categoryId;
+    @NotBlank
+    @Size(min = 4, message = "Min size of category title is 4")
     private String categoryTitle;
+    @NotBlank
+    @Size(min = 10, message = "Min size of category description is 10")
     private String categoryDescription;
 }
